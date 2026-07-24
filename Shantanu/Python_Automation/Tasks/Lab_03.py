8e5b350ad467c19b6ff0523ccc4b73292ea87a73
Shantanoo33 <shantanu440v@gmail.com>
2026-07-24 22:05:15 +0530

    minor changes

--- Shantanu/Python_Automation/Tasks/Lab_03.py
+++ Shantanu/Python_Automation/Tasks/Lab_03.py
@@ -1,11 +1,11 @@
 # exampl1 - To validate the users age is able for voting
 
 
-#age=int(input("Enter the age"))
-#if age>=18:
-  #  print("User is eligible for voting")
-#else :
- #       print("User is not eligible for voting")
+age=int(input("Enter the age"))
+if age>=18:
+   print("User is eligible for voting")
+else :
+        print("User is not eligible for voting")
 
 # example2-  To check given number is positive or negative
 
