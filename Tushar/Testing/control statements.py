d4d44842246697029332571282027094c8e4ab8f
TusharScript <tajadhav1998@gmail.com>
2026-07-26 19:45:59 +0530

    code changes

---
+++ Tushar/Testing/control statements.py
@@ -0,0 +1,56 @@
+# Conditional statements (if, if__else, elif)
+# a=int(input("enter the number"))
+# if a>10:
+#     print("a is grater ")
+# else:
+#     print("a is smaller")
+
+# Find the marks
+# b=int(input("enter the marks"))
+# if b>100:
+#     print("marks are invalid")
+# if b>=90 and b<=100:
+#     print("student have distinction ")
+# elif b>=70 and b<90:
+#     print("student have first class")
+# elif b>=50 and b<70:
+#     print("student have second class")
+# elif b<50:
+#     print("student is failed")
+
+# 2. Looping (Iteration) Statements
+# These statements repeat a block of code.
+
+# Use the while Loop
+# c=int(input("eneter the number"))
+# while c<=10:
+#     print(c)
+#     c+=1
+
+# Use the For loop
+# for i in range(1,21,2):
+#     print(i)
+#
+# 3. Jump (Transfer) Statements
+# These statements alter the normal flow of loops.
+
+# break Statement
+# Terminates the loop immediately.
+# for i in range(1,20):
+#     if i==5:
+#         break
+#     print(i)
+
+# continue Statement
+# Skips the current iteration and continues with the next one.
+# for i in range(1,6):
+#     if i==3:
+#         continue
+#     print(i)
+
+# pass Statement
+# Acts as a placeholder when no action is required.
+# for i in range(1,6):
+#     if i==3:
+#         pass
+#     print(i)
