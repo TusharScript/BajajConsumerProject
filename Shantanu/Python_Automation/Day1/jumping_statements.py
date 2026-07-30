e431b8276efccc639e3794fcc07603d42c0b0cee
Shantanoo33 <shantanu440v@gmail.com>
2026-07-30 23:34:41 +0530

    minor changes

---
+++ Shantanu/Python_Automation/Day1/jumping_statements.py
@@ -0,0 +1,20 @@
+# break:
+#when i = 5 break the loop:
+# for i in range(1,20):
+#     if i==5:
+#         break
+#     print(i)        #it will exit before 5(1,2,3,4)
+# print("Exited!!")
+
+# when i is 5 skip the 5:
+# for i in range(1,11):
+#     if i==5:
+#         continue
+#     print(i)  #(1,2,3,4,6,7,8,...)
+
+# when i is 3,5,7 skip
+for i in range(0,11):
+    if i==3:
+        continue
+        print(i) # ask mahesh about this o/p
+
