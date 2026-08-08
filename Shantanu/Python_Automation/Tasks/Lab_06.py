48de5632b4ba941a6fcf1503f12ad752e4f99082
Shantanoo33 <shantanu440v@gmail.com>
2026-08-09 02:11:23 +0530

    minor changes

---
+++ Shantanu/Python_Automation/Tasks/Lab_06.py
@@ -0,0 +1,17 @@
+#  print all even numbers from 1-100
+
+# for i in range(0,100,2):
+#     print(i)
+
+# for i in range(0,100):
+#     if i%2==0:
+#         print(i)
+
+## print multiplication for a number upto 10
+# a = 2
+# for i in range(1, 11):
+#     b = a * i
+#     print(b)
+
+## reverse a number 123
+
